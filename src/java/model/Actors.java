dc172bc4b159147eac850d401bc56ac2e614eb75
Chun-Ting Yang <chun-ting-yang@uiowa.edu>
2015-10-15 23:38:14 -0500

    Modified Actors.java

--- src/java/model/Actors.java
+++ src/java/model/Actors.java
@@ -13,6 +13,7 @@ public class Actors {
         this.age = 0;
         this.birthday = "";
         this.nationality = "";
+    }
     
     public Actors(int actorsID, String actorsName, int age, String birthday, String nationality) {
         this.actorsID = actorsID;
@@ -21,6 +22,51 @@ public class Actors {
         this.birthday = birthday;
         this.nationality = nationality;
     }
-    
-    
+
+    public int getActorsID() {
+        return actorsID;
+    }
+
+    public void setActorsID(int actorsID) {
+        this.actorsID = actorsID;
+    }
+
+    public String getActorsName() {
+        return actorsName;
+    }
+
+    public void setActorsName(String actorsName) {
+        this.actorsName = actorsName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    @Override
+    public String toString() {
+        return "Actors{" + "actorsID=" + actorsID + ", actorsName=" + actorsName + ", age=" + age + ", birthday=" + birthday + ", nationality=" + nationality + '}';
+    }
+
+
 }
